e706af598604138d0f903519bb904c2b5a155032
Dinhan123456 <chimbuon3@gmail.com>
2025-09-17 10:28:00 +0700

    onecommit

--- src/main/java/com/example/backend/dto/request/ShareWalletRequest.java
+++ src/main/java/com/example/backend/dto/request/ShareWalletRequest.java
@@ -2,13 +2,10 @@ package com.example.backend.dto.request;
 
 import com.example.backend.entity.WalletShare;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -25,5 +22,5 @@ public class ShareWalletRequest {
     private WalletShare.PermissionLevel permissionLevel;
 
     private String message;
-    private LocalDateTime expiryDate;
+    private String expiryDate;
 }
