ef2f28f5d1ad7ed68a992c9d530912e971526d2f
Nguyễn Thị Yến Nhi <131598532+Yenhi6886@users.noreply.github.com>
2025-09-18 16:59:02 +0700

    Revert "Holdup"

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -3,14 +3,12 @@ package com.example.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import com.example.backend.config.AppProperties;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableConfigurationProperties(AppProperties.class)
-@EnableScheduling
 public class BackendApplication {
 
     public static void main(String[] args) {
